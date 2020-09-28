41cf2b1a34d07c1803f4006887709b3ccb5f2428
Kartikey2805 <kartikeygoel093@gmail.com>
2020-09-28 23:25:47 +0530

    Updates

---
+++ precache-manifest.07f5ea1353ac34977ad69ba4547864c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90d7b06faeac1b5d9f9ab3917101e16",
+    "url": "/PhotoWall-App/index.html"
+  },
+  {
+    "revision": "57de76533ae114977091",
+    "url": "/PhotoWall-App/static/css/main.65cba211.chunk.css"
+  },
+  {
+    "revision": "cb5eef8758cd618d41fe",
+    "url": "/PhotoWall-App/static/js/2.f0496ba0.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/PhotoWall-App/static/js/2.f0496ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57de76533ae114977091",
+    "url": "/PhotoWall-App/static/js/main.590d61bc.chunk.js"
+  },
+  {
+    "revision": "973cc49fc5b75d82c806",
+    "url": "/PhotoWall-App/static/js/runtime-main.6540717c.js"
+  }
+]);
